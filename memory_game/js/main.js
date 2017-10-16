c1b84ad90243e27275e2ad495188796ce0834805
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-10-16 01:55:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,9 +29,13 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 };
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId])
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 };
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
 flipCard(2);
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
